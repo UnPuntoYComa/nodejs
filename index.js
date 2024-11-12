a58e96bfad8f91294f67af015c1162ebfe819cbd
Jonathan Solares <jonathansolares1.js1@gmail.com>
2024-11-12 11:05:41 -0600

    prueba

--- index.js
+++ index.js
@@ -4,63 +4,96 @@ const multer = require('multer');
 const cors = require('cors');
 const app = express();
 const port = 3000;
+
 app.use(cors());
-const storage = multer.memoryStorage(); // Configuramos multer para almacenar el archivo en memoria
+app.use(express.json());
+
+// Configuración de multer para almacenar archivos en memoria
+const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
+// Configuración de la base de datos
 const db = mysql.createConnection({
-  host: '65.109.88.87', // Corrige "locahost" a "localhost"
+  host: '65.109.88.87',
   user: 'guaterep_dieg_admin',
   password: 'ZXzavvY]Wo(l',
   database: 'guaterep_bodascarlette'
 });
 
+// Conectar a la base de datos
 db.connect((err) => {
-  if (err) throw err;
+  if (err) {
+    console.error('Error al conectar a la base de datos: ', err);
+    process.exit(1);
+  }
   console.log('Conexión establecida con la base de datos MySQL');
 });
 
-app.use(express.json());
-app.get('/api/invitado/:codigo_invitado', (req, res) => {
-    const codigoInvitado = req.params.codigo_invitado;
-    db.query('SELECT * FROM invitado WHERE codigo_invitacion = ?', codigoInvitado, (error, results) => {
-      if (error) {
-        console.log(error);
-        res.status(500).send('Error al obtener el invitado.');
-      } else if (results.length === 0) {
-        res.status(404).send('El invitado no existe.');
-      } else {
-        res.send(results[0]);
-      }
-    });
-  });
-  app.put('/api/invitadoPut', (req, res) => {
-    const { codigoInvitacion, cantidadConfirmado } = req.body;
-    const sql = 'UPDATE invitado SET confirmacion = 1, confirmacion_cantidad = ? WHERE codigo_invitacion = ?';
-    const values = [cantidadConfirmado, codigoInvitacion];
-    
-    db.query(sql, values, (err, result) => {
+// Reintentar conexión en caso de error
+db.on('error', (err) => {
+  if (err.code === 'PROTOCOL_ENQUEUE_AFTER_FATAL_ERROR') {
+    console.log('Reintentando la conexión a la base de datos...');
+    db.connect((err) => {
       if (err) {
-        console.log(err);
-        res.status(500).send('Error al actualizar la tabla invitado');
+        console.error('Error al reconectar:', err);
       } else {
-        res.json({ id: codigoInvitacion });
+        console.log('Conexión restaurada');
       }
     });
+  }
+});
+
+// Endpoints API
+
+// Obtener invitado por código
+app.get('/api/invitado/:codigo_invitado', (req, res) => {
+  const codigoInvitado = req.params.codigo_invitado;
+  db.query('SELECT * FROM invitado WHERE codigo_invitacion = ?', [codigoInvitado], (error, results) => {
+    if (error) {
+      console.error('Error al obtener el invitado: ', error);
+      return res.status(500).send('Error al obtener el invitado.');
+    } 
+    if (results.length === 0) {
+      return res.status(404).send('El invitado no existe.');
+    }
+    res.send(results[0]);
   });
+});
+
+// Actualizar la confirmación de un invitado
+app.put('/api/invitadoPut', (req, res) => {
+  const { codigoInvitacion, cantidadConfirmado } = req.body;
+  const sql = 'UPDATE invitado SET confirmacion = 1, confirmacion_cantidad = ? WHERE codigo_invitacion = ?';
+  const values = [cantidadConfirmado, codigoInvitacion];
+
+  db.query(sql, values, (err, result) => {
+    if (err) {
+      console.error('Error al actualizar el invitado: ', err);
+      return res.status(500).send('Error al actualizar la tabla invitado');
+    }
+    res.json({ id: codigoInvitacion });
+  });
+});
+
+// Obtener todos los invitados
 app.get('/api/invitados', (req, res) => {
-    db.query('SELECT * FROM invitado', (error, results, fields) => {
-      if (error) {
-        console.error(error);
-        res.status(500).send('Error interno del servidor');
-      } else {
-        res.status(200).json(results);
-      }
-    });
+  db.query('SELECT * FROM invitado', (error, results) => {
+    if (error) {
+      console.error('Error al obtener los invitados: ', error);
+      return res.status(500).send('Error interno del servidor');
+    }
+    res.status(200).json({ message: 'Lista de invitados obtenida correctamente', data: results });
   });
+});
+
+// Crear un nuevo invitado
 app.post('/api/invitado', (req, res) => {
-  const { cantidadInvitado,nombre } = req.body;
-console.log(cantidadInvitado,nombre,"a");
+  const { cantidadInvitado, nombre } = req.body;
+
+  if (!cantidadInvitado || !nombre) {
+    return res.status(400).send('Faltan datos requeridos');
+  }
+
   function generarCodigoInvitacion(callback) {
     const caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
     let codigo = "";
@@ -68,57 +101,64 @@ console.log(cantidadInvitado,nombre,"a");
       codigo += caracteres.charAt(Math.floor(Math.random() * caracteres.length));
     }
     const query = "SELECT codigo_invitacion FROM invitado WHERE codigo_invitacion = ?";
-    const values = [codigo];
-    db.query(query, values, (err, result) => {
+    db.query(query, [codigo], (err, result) => {
       if (err) throw err;
       if (result.length === 0) {
-        // El código no existe en la tabla, es único
         callback(codigo);
       } else {
-        // El código ya existe en la tabla, generamos otro código
         generarCodigoInvitacion(callback);
       }
     });
   }
 
-  
   generarCodigoInvitacion((codigoInvitacion) => {
-    const query = "INSERT INTO invitado (cantidad_invitado,nombre, codigo_invitacion) VALUES (?,?, ?)";
-    const values = [cantidadInvitado,nombre, codigoInvitacion];
-    db.query(query, values, (err, result) => {
-      if (err) throw err;
+    const query = "INSERT INTO invitado (cantidad_invitado, nombre, codigo_invitacion) VALUES (?, ?, ?)";
+    db.query(query, [cantidadInvitado, nombre, codigoInvitacion], (err, result) => {
+      if (err) {
+        console.error('Error al insertar el invitado: ', err);
+        return res.status(500).send('Error al insertar el invitado');
+      }
       console.log(`Se ha insertado un nuevo registro con código de invitación ${codigoInvitacion}`);
       res.status(201).json({
         message: `Se ha insertado un nuevo registro con código de invitación ${codigoInvitacion}`,
-        codigoInvitacion:codigoInvitacion
+        codigoInvitacion
       });
     });
   });
 });
+
+// Crear un mensaje con un archivo adjunto
 app.post('/api/mensaje', upload.single('file'), (req, res) => {
   const { nombre, informacion, mensaje } = req.body;
-  console.log(req.file);
-  const imagen = req.file ? req.file.buffer  : null;
-  const sql = `INSERT INTO mensaje (nombre, imagen_invitado, informacion, mensaje) VALUES (?, ?, ?, ?)`;
-  const values = [nombre, imagen, informacion, mensaje];
+  
+  if (!req.file) {
+    return res.status(400).send('No se ha subido ningún archivo');
+  }
 
-  db.query(sql, values, (err, result) => {
-    if (err) throw err;
+  const imagen = req.file.buffer;
+  const sql = `INSERT INTO mensaje (nombre, imagen_invitado, informacion, mensaje) VALUES (?, ?, ?, ?)`;
+  db.query(sql, [nombre, imagen, informacion, mensaje], (err, result) => {
+    if (err) {
+      console.error('Error al insertar el mensaje: ', err);
+      return res.status(500).send('Error al insertar el mensaje');
+    }
     console.log(`Mensaje creado con ID ${result.insertId}`);
     res.json({ id: result.insertId });
-  });  
+  });
 });
+
+// Obtener todos los mensajes
 app.get('/api/mensajes', (req, res) => {
-        db.query('SELECT * FROM mensaje', (error, results) => {
-          if (error) {
-            console.log(error);
-            res.status(500).send('Error al obtener los mensajes.');
-          } else {
-            res.json({ id: results });
-          }
-        });
-      });
-      
+  db.query('SELECT * FROM mensaje', (error, results) => {
+    if (error) {
+      console.error('Error al obtener los mensajes: ', error);
+      return res.status(500).send('Error al obtener los mensajes');
+    }
+    res.json({ id: results });
+  });
+});
+
+// Iniciar el servidor
 app.listen(port, () => {
   console.log(`Servidor iniciado en http://localhost:${port}`);
 });
