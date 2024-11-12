5da87a24c3acf2dd3a2e762097ec7c74a87a1dc7
Jonathan Solares <jonathansolares1.js1@gmail.com>
2024-11-12 13:16:36 -0600

    prueba

--- index.js
+++ index.js
@@ -13,26 +13,33 @@ const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
 // Configuración de la base de datos
-const dbConfig = {
+const db = mysql.createConnection({
   host: '65.109.88.87',
   user: 'guaterep_dieg_admin',
   password: 'ZXzavvY]Wo(l',
   database: 'guaterep_bodascarlette'
-};
+});
 
-// Función para crear una nueva conexión
-function createDbConnection() {
-  return mysql.createConnection(dbConfig);
-}
-
-// Conectar a la base de datos (inicial)
-let db = createDbConnection();
+// Conectar a la base de datos
+db.connect((err) => {
+  if (err) {
+    console.error('Error al conectar a la base de datos: ', err);
+    process.exit(1);
+  }
+  console.log('Conexión establecida con la base de datos MySQL');
+});
 
 // Reintentar conexión en caso de error
 db.on('error', (err) => {
   if (err.code === 'PROTOCOL_ENQUEUE_AFTER_FATAL_ERROR') {
     console.log('Reintentando la conexión a la base de datos...');
-    db = createDbConnection(); // Crear una nueva conexión
+    db.connect((err) => {
+      if (err) {
+        console.error('Error al reconectar:', err);
+      } else {
+        console.log('Conexión restaurada');
+      }
+    });
   }
 });
 
@@ -41,22 +48,15 @@ db.on('error', (err) => {
 // Obtener invitado por código
 app.get('/api/invitado/:codigo_invitado', (req, res) => {
   const codigoInvitado = req.params.codigo_invitado;
-  db.connect((err) => {
-    if (err) {
-      console.error('Error al conectar a la base de datos: ', err);
+  db.query('SELECT * FROM invitado WHERE codigo_invitacion = ?', [codigoInvitado], (error, results) => {
+    if (error) {
+      console.error('Error al obtener el invitado: ', error);
       return res.status(500).send('Error al obtener el invitado.');
+    } 
+    if (results.length === 0) {
+      return res.status(404).send('El invitado no existe.');
     }
-    db.query('SELECT * FROM invitado WHERE codigo_invitacion = ?', [codigoInvitado], (error, results) => {
-      db.end(); // Cerrar la conexión después de la consulta
-      if (error) {
-        console.error('Error al obtener el invitado: ', error);
-        return res.status(500).send('Error al obtener el invitado.');
-      }
-      if (results.length === 0) {
-        return res.status(404).send('El invitado no existe.');
-      }
-      res.send(results[0]);
-    });
+    res.send(results[0]);
   });
 });
 
@@ -66,37 +66,23 @@ app.put('/api/invitadoPut', (req, res) => {
   const sql = 'UPDATE invitado SET confirmacion = 1, confirmacion_cantidad = ? WHERE codigo_invitacion = ?';
   const values = [cantidadConfirmado, codigoInvitacion];
 
-  db.connect((err) => {
+  db.query(sql, values, (err, result) => {
     if (err) {
-      console.error('Error al conectar a la base de datos: ', err);
-      return res.status(500).send('Error al actualizar el invitado.');
+      console.error('Error al actualizar el invitado: ', err);
+      return res.status(500).send('Error al actualizar la tabla invitado');
     }
-    db.query(sql, values, (err, result) => {
-      db.end(); // Cerrar la conexión después de la consulta
-      if (err) {
-        console.error('Error al actualizar el invitado: ', err);
-        return res.status(500).send('Error al actualizar la tabla invitado');
-      }
-      res.json({ id: codigoInvitacion });
-    });
+    res.json({ id: codigoInvitacion });
   });
 });
 
 // Obtener todos los invitados
 app.get('/api/invitados', (req, res) => {
-  db.connect((err) => {
-    if (err) {
-      console.error('Error al conectar a la base de datos: ', err);
-      return res.status(500).send('Error al obtener los invitados.');
+  db.query('SELECT * FROM invitado', (error, results) => {
+    if (error) {
+      console.error('Error al obtener los invitados: ', error);
+      return res.status(500).send('Error interno del servidor');
     }
-    db.query('SELECT * FROM invitado', (error, results) => {
-      db.end(); // Cerrar la conexión después de la consulta
-      if (error) {
-        console.error('Error al obtener los invitados: ', error);
-        return res.status(500).send('Error interno del servidor');
-      }
-      res.status(200).json(results);
-    });
+    res.status(200).json({ message: 'Lista de invitados obtenida correctamente', data: results });
   });
 });
 
@@ -115,40 +101,27 @@ app.post('/api/invitado', (req, res) => {
       codigo += caracteres.charAt(Math.floor(Math.random() * caracteres.length));
     }
     const query = "SELECT codigo_invitacion FROM invitado WHERE codigo_invitacion = ?";
-    db.connect((err) => {
-      if (err) {
-        console.error('Error al conectar a la base de datos: ', err);
-        return res.status(500).send('Error al generar código de invitación');
+    db.query(query, [codigo], (err, result) => {
+      if (err) throw err;
+      if (result.length === 0) {
+        callback(codigo);
+      } else {
+        generarCodigoInvitacion(callback);
       }
-      db.query(query, [codigo], (err, result) => {
-        db.end();
-        if (err) throw err;
-        if (result.length === 0) {
-          callback(codigo);
-        } else {
-          generarCodigoInvitacion(callback);
-        }
-      });
     });
   }
 
   generarCodigoInvitacion((codigoInvitacion) => {
     const query = "INSERT INTO invitado (cantidad_invitado, nombre, codigo_invitacion) VALUES (?, ?, ?)";
-    db.connect((err) => {
+    db.query(query, [cantidadInvitado, nombre, codigoInvitacion], (err, result) => {
       if (err) {
-        console.error('Error al conectar a la base de datos: ', err);
+        console.error('Error al insertar el invitado: ', err);
         return res.status(500).send('Error al insertar el invitado');
       }
-      db.query(query, [cantidadInvitado, nombre, codigoInvitacion], (err, result) => {
-        db.end(); // Cerrar la conexión después de la consulta
-        if (err) {
-          console.error('Error al insertar el invitado: ', err);
-          return res.status(500).send('Error al insertar el invitado');
-        }
-        console.log(`Se ha insertado un nuevo registro con código de invitación ${codigoInvitacion}`);
-        res.status(201).json({
-          message: `Se ha insertado un nuevo registro con código de invitación ${codigoInvitacion}`
-        });
+      console.log(`Se ha insertado un nuevo registro con código de invitación ${codigoInvitacion}`);
+      res.status(201).json({
+        message: `Se ha insertado un nuevo registro con código de invitación ${codigoInvitacion}`,
+        codigoInvitacion
       });
     });
   });
@@ -164,39 +137,24 @@ app.post('/api/mensaje', upload.single('file'), (req, res) => {
 
   const imagen = req.file.buffer;
   const sql = `INSERT INTO mensaje (nombre, imagen_invitado, informacion, mensaje) VALUES (?, ?, ?, ?)`;
-
-  db.connect((err) => {
+  db.query(sql, [nombre, imagen, informacion, mensaje], (err, result) => {
     if (err) {
-      console.error('Error al conectar a la base de datos: ', err);
+      console.error('Error al insertar el mensaje: ', err);
       return res.status(500).send('Error al insertar el mensaje');
     }
-    db.query(sql, [nombre, imagen, informacion, mensaje], (err, result) => {
-      db.end(); // Cerrar la conexión después de la consulta
-      if (err) {
-        console.error('Error al insertar el mensaje: ', err);
-        return res.status(500).send('Error al insertar el mensaje');
-      }
-      console.log(`Mensaje creado con ID ${result.insertId}`);
-      res.json({ id: result.insertId });
-    });
+    console.log(`Mensaje creado con ID ${result.insertId}`);
+    res.json({ id: result.insertId });
   });
 });
 
 // Obtener todos los mensajes
 app.get('/api/mensajes', (req, res) => {
-  db.connect((err) => {
-    if (err) {
-      console.error('Error al conectar a la base de datos: ', err);
+  db.query('SELECT * FROM mensaje', (error, results) => {
+    if (error) {
+      console.error('Error al obtener los mensajes: ', error);
       return res.status(500).send('Error al obtener los mensajes');
     }
-    db.query('SELECT * FROM mensaje', (error, results) => {
-      db.end(); // Cerrar la conexión después de la consulta
-      if (error) {
-        console.error('Error al obtener los mensajes: ', error);
-        return res.status(500).send('Error al obtener los mensajes');
-      }
-      res.json({ id: results });
-    });
+    res.json({ id: results });
   });
 });
 
